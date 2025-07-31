bf07cd6ec64a9d47eb706fbc6fd6d41752d6dc10
joaoporto27 <joaovitorportosales46@gmail.com>
2025-07-31 08:26:28 -0300

    Feat: dupla monstra e cadu

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Tab from "./navigation/TabNavigator.js"
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Tab />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
